2807f5c7bc9d970ebabc71dff4ba266be70a6cf1
Barry Lee <bclee@buffalo.edu>
2017-12-05 14:03:56 -0500

    Automation Included
    lib.sh is a shell script for changing directories and executing main.py
    Updated README.md for installation instructions for crontab.
    crontab_example is what should be inside crontab after pasting the command after "crontab -e"

---
+++ lib.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+PATH=/Library/Frameworks/Python.framework/Versions/3.5/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
+cd /'''Directory to main.py'''/
+echo "Beginning Reservations"
+date +"%T"
+python main.py
